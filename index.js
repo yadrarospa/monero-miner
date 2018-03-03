6113642784d7365f3e60909581048171cf674042
yadrarospa <37023068+yadrarospa@users.noreply.github.com>
2018-03-03 13:10:56 -0400

    Update 3318

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('3V9OwXcetqh2sNaqrgBvdoEezOaXd0Q6'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
